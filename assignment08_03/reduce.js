5001659a2d00d06879c4f81e8ad729f15c35883b
biggochu <118151034+biggochu@users.noreply.github.com>
2022-12-11 07:56:08 -0800

    assignment08_03 progress update

--- assignment08_03/reduce.js
+++ assignment08_03/reduce.js
@@ -6,7 +6,9 @@ Examples:
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {
+  return arr.reduce((acc, val) => [...acc, val[key]], [])
+}
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
@@ -19,7 +21,22 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+  return Array.from(str).reduce(
+    (acc, val) => {
+      if (!isVowel(val)) {
+        return acc
+      }
+
+      if (val in acc) {
+        acc[val]++
+      } else {
+        acc[val] = 1
+      }
+      return acc
+    }, {}
+  )
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
@@ -36,7 +53,14 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+  return arr.reduce(
+    (acc, val) => {
+      val[key] = value
+      return [...acc, val]
+    }, []
+  )
+}
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
@@ -60,4 +84,22 @@ Examples:
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
-function partition(arr, callback) {}
+function partition(arr, callback) {
+  return arr.reduce(
+    (acc, val) => {
+      if (callback(val)) {
+        acc[0].push(val)
+      } else {
+        acc[1].push(val)
+      }
+      return acc
+    }, [
+      [],
+      []
+    ]
+  )
+}
+
+function isVowel(c) {
+  return c === 'a' || c === 'e' || c === 'i' || c === 'o' || c === 'u'
+}
