a0d8c7aee69cda16c40009a0b3e9e808a5a14207
biggochu <118151034+biggochu@users.noreply.github.com>
2022-12-10 22:24:33 -0800

    assignment07_01 progress update

--- assignment07_01/tip-pool/payments.test.js
+++ assignment07_01/tip-pool/payments.test.js
@@ -43,9 +43,95 @@ describe("payments tests", () => {
     })
 
     afterAll(() => {
+      billAmtInput.value = ''
+      tipAmtInput.value = ''
       allPayments = {}
       paymentTbody.innerHTML = ''
       updateSummary()
+    })
+  })
+
+  describe("submitPaymentInfo()", () => {
+    beforeEach(() => {
+      billAmtInput.value = 100
+      tipAmtInput.value = 20
+      paymentId = 0
+      allPayments = {}
+    })
+
+    it('should increment paymentId if curPayment is valid', () => {
+      submitPaymentInfo()
+      expect(paymentId).toEqual(1)
+    })
+
+    it('should set the curPayment object in allPayments', () => {
+      submitPaymentInfo()
+      expect(allPayments['payment' + paymentId]).toEqual({
+        billAmt: '100',
+        tipAmt: '20',
+        tipPercent: 20
+      })
+    })
+
+    afterEach(() => {
+      billAmtInput.value = ''
+      tipAmtInput.value = ''
+    })
+  })
+
+  describe("appendPaymentTable()", () => {
+    let curPayment = {
+      billAmt: '100',
+      tipAmt: '20',
+      tipPercent: 20
+    }
+
+    beforeEach(() => {
+      paymentTbody.innerHTML = ''
+    })
+
+    it('should append a new payment row to payments table', () => {
+      appendPaymentTable(curPayment)
+
+      const $tr = paymentTbody.children[0]
+
+      expect($tr.tagName).toEqual('TR')
+      expect($tr.children.length).toEqual(3)
+      expect($tr.children[0].innerText).toEqual('$100')
+      expect($tr.children[1].innerText).toEqual('$20')
+      expect($tr.children[2].innerText).toEqual('20%')
+    })
+
+    afterEach(() => {
+      paymentTbody.innerHTML = ''
+    })
+  })
+
+  describe("updateSummary()", () => {
+    beforeEach(() => {
+      paymentId = 0
+      allPayments = {}
+    })
+
+    it('should update the summary table cells', () => {
+      billAmtInput.value = 100
+      tipAmtInput.value = 20
+      submitPaymentInfo()
+
+      billAmtInput.value = 120
+      tipAmtInput.value = 5
+      submitPaymentInfo()
+
+      updateSummary()
+
+      expect(summaryTds[0].innerText).toEqual('$220')
+      expect(summaryTds[1].innerText).toEqual('$25')
+      expect(summaryTds[2].innerText).toEqual('12%')
+    })
+
+    afterEach(() => {
+      billAmtInput.value = ''
+      tipAmtInput.value = ''
     })
   })
 })
--- assignment07_01/tip-pool/servers.test.js
+++ assignment07_01/tip-pool/servers.test.js
@@ -11,14 +11,26 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should not add a new server if server name is empty', () => {
+    serverNameInput.value = ''
+    submitServerInfo()
+
+    expect(Object.keys(allServers).length).toEqual(0)
+  })
+
   it('should add the new server to html table on updateServerTable()', () => {
+    submitServerInfo()
     updateServerTable()
+
+    const $tr = serverTbody.querySelector('tr:last-child')
+    expect($tr.children[0].innerText).toEqual('Alice')
+    expect($tr.children[1].innerText).toEqual('$50.00')
   })
 
   afterEach(function() {
     serverNameInput.value = ''
     allServers = {}
     serverId = 0
-    updateServerTable()
+    serverTbody.innerHTML = '';
   });
 });
