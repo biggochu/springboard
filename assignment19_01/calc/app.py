811b3d930d43601cf4845e02a50a1e28f60921e5
biggochu <118151034+biggochu@users.noreply.github.com>
2023-01-15 04:38:49 -0800

    fix /math path

--- assignment19_01/calc/app.py
+++ assignment19_01/calc/app.py
@@ -7,24 +7,32 @@ app = Flask(__name__)
 
 
 @app.route("/add")
-@app.route("/math/add")
 def calc_add():
     return str(operations.add(int(request.args["a"]), int(request.args["b"])))
 
 
 @app.route("/sub")
-@app.route("/math/sub")
 def calc_subtract():
     return str(operations.sub(int(request.args["a"]), int(request.args["b"])))
 
 
 @app.route("/mult")
-@app.route("/math/mult")
 def calc_multiply():
     return str(operations.mult(int(request.args["a"]), int(request.args["b"])))
 
 
 @app.route("/div")
-@app.route("/math/div")
 def calc_divide():
     return str(operations.div(int(request.args["a"]), int(request.args["b"])))
+
+
+@app.route("/math/<op>")
+def calc_all(op):
+    if op == "add":
+        return str(operations.add(int(request.args["a"]), int(request.args["b"])))
+    elif op == "sub":
+        return str(operations.sub(int(request.args["a"]), int(request.args["b"])))
+    elif op == "mult":
+        return str(operations.mult(int(request.args["a"]), int(request.args["b"])))
+    elif op == "div":
+        return str(operations.div(int(request.args["a"]), int(request.args["b"])))
