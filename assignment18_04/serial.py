e08cee402f7d25ff0577032e98e65d5dbb6b3a2c
biggochu <118151034+biggochu@users.noreply.github.com>
2023-01-14 19:59:49 -0800

    assignment18_04 progress update

--- assignment18_04/serial.py
+++ assignment18_04/serial.py
@@ -1,8 +1,9 @@
 """Python serial number generator."""
 
+
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -20,3 +21,16 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start=0):
+        self.start = start
+        self.num = start
+
+    def generate(self):
+        """Increment num and return value before increment"""
+        num = self.num
+        self.num = self.num + 1
+        return num
+
+    def reset(self):
+        """Reset num to start value"""
+        self.num = self.start
--- assignment18_04/wordfinder.py
+++ assignment18_04/wordfinder.py
@@ -1,5 +1,62 @@
 """Word Finder: finds random words from a dictionary."""
+import random
 
 
 class WordFinder:
-    ...
+    """
+    Loads a list of words from a file and allows selection of a random word
+
+    >>> wf = WordFinder("words.txt")
+    235886 words read
+
+    >>> alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    >>> word = wf.random()
+    >>> [True if c in alphabet else False for c in word.lower()].count(True) == len(word)
+    True
+
+    >>> len(word) > 0
+    True
+    """
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.words = []
+        self.load(filepath)
+
+    def load(self, filepath):
+        """load list of words from specified filepath"""
+        try:
+            with open(filepath) as f:
+                self.words = [line.rstrip() for line in f.readlines()]
+                print(f"{len(self.words)} words read")
+        except:
+            print("Unable to load file")
+
+    def random(self):
+        """select a random word from word list"""
+        index = random.randint(0, len(self.words) - 1)
+        return self.words[index]
+
+
+class SpecialWordFinder(WordFinder):
+    """
+    Loads a list of words from a file and allows selection of a random word
+
+    >>> swf = SpecialWordFinder("special-words.txt")
+    235886 words read by SpecialWordFinder
+    """
+
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def load(self, filepath):
+        """load list of words from sepcified filepath excluding blank lines and comments"""
+        try:
+            with open(filepath) as f:
+                for line in f.readlines():
+                    line = line.rstrip().lstrip()
+                    if len(line) > 0 and line[0] != "#":
+                        self.words.append(line)
+                print(f"{len(self.words)} words read by SpecialWordFinder")
+        except:
+            print("Unable to load file")
